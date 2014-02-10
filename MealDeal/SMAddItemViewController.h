c5c8606540e4ad024ba44ef3ff9f737cc055103f
Stephen McDonald <stephenmcdonald@Stephens-MacBook-Pro-2.local>
2014-02-09 22:54:22 -0500

    SM - Added add item view controller

---
+++ MealDeal/SMAddItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMAddItemViewController.h
+//  MealDeal
+//
+//  Created by Stephen McDonald on 2/9/2014.
+//  Copyright (c) 2014 Stephen McDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAddItemViewController : UIViewController
+
+@end
---
+++ MealDeal/SMAddItemViewController.m
@@ -0,0 +1,31 @@
+//
+//  SMAddItemViewController.m
+//  MealDeal
+//
+//  Created by Stephen McDonald on 2/9/2014.
+//  Copyright (c) 2014 Stephen McDonald. All rights reserved.
+//
+
+#import "SMAddItemViewController.h"
+
+@interface SMAddItemViewController ()
+
+@end
+
+@implementation SMAddItemViewController
+
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - IBActions
+
+- (IBAction)cancelButtonTapped:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
